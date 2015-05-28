5e0d2eccd88b4d5e5785c344a6578af818938aff
Karonsbo <karro.jonzen@hotmail.com>
2015-05-28 15:09:22 +0200

    Säger om man gissat rätt eller ej
    kör alert bara nu så ser man om man gissat rätt! vet inte hur hårda vi
    ska va med bedömningen!! ^^

--- Kartastrof/Kartastrof/Controllers/HomeController.cs
+++ Kartastrof/Kartastrof/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using Kartastrof.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Kartastrof.Classes;
 
 namespace Kartastrof.Controllers
 {
@@ -24,6 +26,8 @@ namespace Kartastrof.Controllers
             Random rnd = new Random();
             int randomNr = rnd.Next(0, count);
             Tbl_Capital correctAnswer = db.Tbl_Capital.Find(randomNr);
+            Session["latitude"] = correctAnswer.Ca_Latitude.ToString();
+            Session["longitude"] = correctAnswer.Ca_Longitude.ToString();
 
             ViewBag.Message = "Your application description page.";
 
@@ -34,7 +38,37 @@ namespace Kartastrof.Controllers
         public string Submit(string lat, string lng)
         {
             string answer = "Latitud: "+lat + " " +"Longitud: "+lng;
-            return answer;
+            
+            if (isCorrectAnswer(lat, lng))
+            {
+                return "Grattis! Du gissade rätt! :D";
+            } else{
+                return "Tyvärr! Du gissade fel! Prova igen!!!";
+            }
+            //return answer;
+        }
+
+        public bool isCorrectAnswer(string lat, string lng) {
+            bool isCorrect = false;
+            double span = 0.5;
+
+            double answerLat = double.Parse(lat, CultureInfo.InvariantCulture);
+            double answerLng = double.Parse(lng, CultureInfo.InvariantCulture);
+            double correctLat = double.Parse(Session["latitude"].ToString());
+            double correctLng = double.Parse(Session["longitude"].ToString());
+            /*
+            if (Enumerable.Range(correctLat - span, correctLat + span).Contains(answerLat) && Enumerable.Range(correctLng - span, correctLng + span).Contains(answerLng))
+            {
+                isCorrect = true;
+            }*/
+
+            if (BetweenExtensions.IsBetweenII(answerLat, correctLat - span, correctLat + span) && BetweenExtensions.IsBetweenII(answerLng, correctLng - span, correctLng + span))
+            {
+                isCorrect = true;
+            }
+
+
+            return isCorrect;
         }
 
         public ActionResult Contact()
