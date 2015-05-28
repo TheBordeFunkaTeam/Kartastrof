a6fc69403784e15d8916641363ef374f053aabfb
sandra <sandraingerhage@gmail.com>
2015-05-28 17:08:35 +0200

    merge. added GetClue() in Home, CapitalInfo.cs.

--- Kartastrof/Kartastrof/Controllers/HomeController.cs
+++ Kartastrof/Kartastrof/Controllers/HomeController.cs
@@ -6,6 +6,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Kartastrof.Classes;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Web.Script.Serialization;
 
 namespace Kartastrof.Controllers
 {
@@ -74,6 +78,61 @@ namespace Kartastrof.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GetClue()
+        {
+            string url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext=true&exsectionformat=plain&titles=Helsinki&format=json";
+
+            //Get info from subscribers
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "text/json";
+            httpWebRequest.Method = "GET";
+
+
+            try
+            {
+                //Read response
+                var response = "empty";
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    response = streamReader.ReadToEnd();
+                    streamReader.Close();
+                }
+
+                string[] lines = Regex.Split(response, "{");
+                int i = 0;
+                string serializeResponse = null;
+                foreach (string line in lines)
+                {
+                    if (i == 4)
+                    {
+                        serializeResponse = "{" + line;
+                    }
+                    i++;
+                }
+
+                serializeResponse = serializeResponse.Remove(serializeResponse.Length - 3);
+                //System.Diagnostics.Debug.WriteLine("SERIALIZERESPONE" + serializeResponse);
+
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+                CapitalInfo capInfo = serializer.Deserialize<CapitalInfo>(serializeResponse);
+                System.Diagnostics.Debug.WriteLine("OUT: " + capInfo.title + " EXTRACT " + capInfo.extract);
+
+            }
+            catch
+            {
+
+                System.Diagnostics.Debug.WriteLine("COULD NOT BE FOUND");
+
+            }
+
+
+
 
             return View();
         }
